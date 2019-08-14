92dad23162f97024d98d7d49d225c84e3439b818
David Mordechai <david.mordechai79@gmail.com>
2019-08-14 22:54:23 +0300

    refactoring lcs method and added more unit tests

--- main.js
+++ main.js
@@ -1,20 +1,36 @@
-/*
-'ABAZDC', 'BACBAD' => 'ABAD'
-'AGGTAB', 'GXTXAYB' => 'GTAB'
-'aaaa', 'aa' => 'aa'
-*/
-const FindLongestCommonSequenceAmongTwoString = (s1, s2) => {   
-    const result = new Array();
-    const [st1, st2] = orderTwoStringbyLongestLength(s1, s2);
-    let idx2StartPosition = 0;
-    for(let st1Idx = 0; st1Idx < st1.length && idx2StartPosition < st2.length; st1Idx++){
-        let St2Idx = st2.indexOf(st1[st1Idx], idx2StartPosition);
-        if(St2Idx != -1){
-            result.push(st2[St2Idx]);
-            idx2StartPosition = St2Idx + 1;
-        }   
+// Longest Common Subsequence - Dynamic Programming
+lcs = (s1, s2) => {
+
+    var matrix = [];
+    for (var i = 0; i < s1.length + 1; i++) {
+        matrix[i] = new Array(s2.length + 1);
+    }
+
+    for (let row = 0; row <= s1.length; row++) {
+        for (let column = 0; column <= s2.length; column++) {
+            if (row === 0 || column === 0)
+                matrix[row][column] = 0;
+            else if (s1[row - 1] == s2[column - 1])
+                matrix[row][column] = matrix[row - 1][column - 1] + 1;
+            else
+                matrix[row][column] = Math.max(matrix[row - 1][column], matrix[row][column - 1]);
+        }
+    }
+
+    const lcsLength = matrix[s1.length][s2.length];
+    const result = new Array(lcsLength);
+
+    for (let i = s1.length, j = s2.length, k = lcsLength - 1; k >= 0;) {
+        if (s1[i - 1] == s2[j - 1]) {
+            result[k] = s1[i - 1];
+            i--; j--; k--;
+        }
+        else if (matrix[i][j - 1] > matrix[i - 1][j])
+            j--;
+        else
+            i--;
     }
     return result.join('');
 }
-const orderTwoStringbyLongestLength = (st1, st2) => st1.length >= st2.length ? [st1, st2] : [st2, st1];
-module.exports = [orderTwoStringbyLongestLength, FindLongestCommonSequenceAmongTwoString];
+
+module.exports = lcs;
--- main.test.js
+++ main.test.js
@@ -1,14 +1,36 @@
-const [orderTwoStringbyLongestLength, FindLongestCommonSequenceAmongTwoString] = require('./main');
+const lcs = require('./main');
 
-test('Get longest string between two strings', () =>{
-    expect(orderTwoStringbyLongestLength('aa', 'bbbb')).toEqual(["bbbb", "aa"]);
-    expect(orderTwoStringbyLongestLength('aaaa', 'bb')).toEqual(["aaaa", "bb"]);
-    expect(orderTwoStringbyLongestLength('aaaa', 'bbbb')).toEqual(["aaaa", "bbbb"]);
+test("1.  'ABAZDC'      'BACBAD'    => 'ABAD'", () => {
+    expect(lcs('ABAZDC', 'BACBAD')).toBe("ABAD");
 });
-
-test('Find longest common sequence among 2 string', () =>{
-    expect(FindLongestCommonSequenceAmongTwoString('ABAZDC', 'BACBAD')).toBe("ABAD");
-    expect(FindLongestCommonSequenceAmongTwoString('AGGTAB', 'GXTXAYB')).toBe("GTAB");
-    expect(FindLongestCommonSequenceAmongTwoString('aaaa', 'aa')).toBe("aa");
+test("2.  'AGGTAB'      'GXTXAYB'   => 'GTAB'", () => {
+    expect(lcs('AGGTAB', 'GXTXAYB')).toBe("GTAB");
 });
-
+test("3.  'aaaa'        'aa'        => 'aa'", () => {
+    expect(lcs('aaaa', 'aa')).toBe("aa");
+});
+test("4.  'acbaed'      'abcadf'    => 'acad'", () => {
+    expect(lcs('acbaed', 'abcadf')).toBe("acad");
+});
+test("5.  'dcbaabc'     'abcdcba'   => 'dcba'", () => {
+    expect(lcs('dcbaabc', 'abcdcba')).toBe("dcba");
+});
+test("6.  'ABCDGH'      'AEDFHR'    => 'ADH'", () => {
+    expect(lcs('ABCDGH', 'AEDFHR')).toBe("ADH");
+});
+test("7.  'abcdefghij'  'ecdgi'     => 'cdgi'", () => {
+    expect(lcs('abcdefghij', 'ecdgi')).toBe("cdgi");
+});
+test("8.  'abcefgdx'    'efgdabc'   => 'efgd'", () => {
+    expect(lcs('abcefgdx', 'efgdabc')).toBe("efgd");
+});
+test("9.  'acbad'       'babazdc'   => 'abad'", () => {
+    expect(lcs('acbad', 'babazdc')).toBe("abad");
+});
+test("10. 'A'           'B'         => ''", () => {
+    expect(lcs('A', 'B')).toBe("");
+});
+test("11. 'jklABCDGH'   'AEDFHRjkl' => 'jkl' or 'ADH'", () => {
+    const result = lcs('jklABCDGH', 'AEDFHRjkl');
+    expect(result === 'jkl' || result === 'ADH').toBeTruthy();
+});
